36049dc88e8e1608404b35f4c2432cccb29e173d
Kelli <kellibrookelife@gmail.com>
2018-06-01 12:01:22 -0700

    add click function to submit button, add variables to click function

---
+++ js/scripts.js
@@ -0,0 +1,27 @@
+$(function(){
+  $("#submit").click(function(){
+
+    var question1Input = parseInt($("input:radio[name=question-1]:checked").val());
+    var question2Input = parseInt($("input:radio[name=question-2]:checked").val());
+    var question3Input = parseInt($("input:radio[name=question-3]:checked").val());
+    var question4Input = parseInt($("input:radio[name=question-4]:checked").val());
+
+    var score= question1Input + question2Input + question3Input + question4Input;
+    alert(score);
+
+    var question4Input = $("input:radio[name=question-5]:checked").val();
+    var question5Input = $("input:radio[name=question-6]:checked").val();
+
+    
+
+
+    event.preventDefault();
+  })
+
+
+
+
+
+
+
+})
